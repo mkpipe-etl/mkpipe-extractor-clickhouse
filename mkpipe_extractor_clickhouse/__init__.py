d1ae89ae5ce8478e6f551b88ceeff33aaf04fc4a
m-karakus <s.metinkarakus@gmail.com>
2026-02-09 14:18:17 +0300

    new structure

--- mkpipe_extractor_clickhouse/__init__.py
+++ mkpipe_extractor_clickhouse/__init__.py
@@ -1,11 +1,12 @@
-from mkpipe.functions_spark import BaseExtractor
+from mkpipe.spark import JdbcExtractor
 
 
-class ClickhouseExtractor(BaseExtractor):
-    def __init__(self, config, settings):
-        super().__init__(
-            config,
-            settings,
-            driver_name='clickhouse',
-            driver_jdbc='com.clickhouse.jdbc.ClickHouseDriver',
+class ClickhouseExtractor(JdbcExtractor, variant='clickhouse'):
+    driver_name = 'clickhouse'
+    driver_jdbc = 'com.clickhouse.jdbc.ClickHouseDriver'
+
+    def build_jdbc_url(self):
+        return (
+            f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}'
+            f'?user={self.username}&password={self.password}'
         )
